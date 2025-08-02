e662a705e51ce2cc7e7c50f18d31319c515a82d8
Shubh Narang <narangshubh08@gmail.com>
2025-08-02 20:06:54 +0530

    Second Lecture Done

---
+++ First.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+/*
+    int a = 'a';
+    cout << a << endl;
+    char ch1 = 123456;
+    cout << ch1 <<endl; 
+    char ch = 123456;
+    cout << ch <<endl; 
+
+    int a = 100;
+    cout << a <<endl;
+    char b = 'v' ;
+    cout << b <<endl;
+    bool b1 = true ;
+    cout << b1 <<endl;
+    float f = 1.2;
+    cout << f <<endl;
+    double d = 1.23;
+    cout << d <<endl;
+    int size = sizeof(a);
+    cout << size <<endl;
+    cout << sizeof(a) << endl;
+    cout << "Size of a : " << size << endl;
+
+
+
+    unsigned int a1 = 112;
+    cout << a1 << endl;
+
+    unsigned int a = -112;
+    cout << a << endl;
+
+
+    double a = 2.0/5;
+    cout << a << endl;
+    cout << 2.0/5 << endl;
+
+
+    int a = 5;
+    int b = 4;
+    bool first = (a>b);
+    cout << first << endl;
+
+    bool second = (a<b);
+    cout << second << endl;
+
+    bool third = (a==b);
+    cout << third << endl;
+
+    bool forth = (a>=b);
+    cout << forth << endl;
+    
+*/
+    int a=0;
+    cout << !a <<endl;
+}
---
+++ If_Else.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a ;
+    //  cout << "value of n : " << n <<endl;
+
+    if (a>0) {
+        cout << a << " is a positive number" <<endl;
+    }
+    else {
+        cout << a << " is a negative number" <<endl;
+    }
+}
